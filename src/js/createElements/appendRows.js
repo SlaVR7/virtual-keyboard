9567d91613dd7b819170abdb538604384292f712
SlaVR7 <mylife0793@gmail.com>
2023-10-17 22:27:12 +0300

    Updates

--- src/js/createElements/appendRows.js
+++
@@ -1,9 +0,0 @@
-import keyboardData from '../keyboardData';
-
-function appendRows(rowsArray) {
-  rowsArray.forEach((item) => {
-    keyboardData.keyboard.appendChild(item);
-  });
-}
-
-export default appendRows;
